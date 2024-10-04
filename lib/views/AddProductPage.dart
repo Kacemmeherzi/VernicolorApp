b537b70c838c61675e1b670c617ef029e7a5da7b
kacem <kacemmeherzi52@gmail.com>
2024-10-04 12:13:05 +0100

    select list of product family added in the add product from

--- lib/views/AddProductPage.dart
+++ lib/views/AddProductPage.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
+import 'package:vernicolorapp/models/ProductFamily.dart';
+import 'package:vernicolorapp/services/ProductFamilyService.dart';
 
 class AddProductPage extends StatefulWidget {
   @override
@@ -11,10 +13,33 @@ class AddProductPage extends StatefulWidget {
 
 class _AddProductPageState extends State<AddProductPage> {
   final _formKey = GlobalKey<FormState>();
+  String? _productId ; 
   String? _productName;
   String? _productDescription;
+  String? _productStatus;
   File? _image;
   final ImagePicker _picker = ImagePicker();
+  String? _selectedProductFamily;
+  List<ProductFamily> _productFamilies = [];
+  late ProductFamilyService _productFamilyService ;
+   @override
+  void initState() {
+    super.initState();
+    _productFamilyService = new ProductFamilyService(baseUrl: 'http://10.0.2.2:8082/prodfamily') ;
+    _fetchProductFamilies();
+  }
+   Future<void> _fetchProductFamilies() async {
+    try {
+      List<ProductFamily> families = await _productFamilyService.getProductFamilies();
+      setState(() {
+        _productFamilies = families;
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to load product families')),
+      );
+    }
+  }
 
   // Method to pick an image from the gallery
   Future<void> _pickImage() async {
@@ -42,14 +67,14 @@ class _AddProductPageState extends State<AddProductPage> {
       // Create a multipart request
       var request = http.MultipartRequest(
         'POST',
-        Uri.parse('http://localhost:8080/products'), // Update with your API endpoint
+        Uri.parse('http://10.0.2.2:8082/api/products/createp'), // Update with your API endpoint
       );
 
       // Add fields for product name and description
       request.fields['name'] = _productName!;
+      request.fields['status'] = _productStatus!;
       request.fields['description'] = _productDescription!;
-      request.fields['description'] = _productDescription!;
-      request.fields['description'] = _productDescription!;
+      request.fields['prodfamily'] = _selectedProductFamily!;
 
 
       
@@ -57,7 +82,7 @@ class _AddProductPageState extends State<AddProductPage> {
       // Attach image file
       request.files.add(
         await http.MultipartFile.fromPath(
-          'image', // This should match the @RequestParam name in Spring Boot
+          'image', 
           _image!.path,
         ),
       );
@@ -66,9 +91,10 @@ class _AddProductPageState extends State<AddProductPage> {
       var response = await request.send();
 
       // Handle response
-      if (response.statusCode == 200) {
+      if (response.statusCode == 201) {
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Product added successfully')),
+          SnackBar(content: Text('Product added successfully'),backgroundColor: Colors.green,),
+
         );
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
@@ -84,10 +110,53 @@ class _AddProductPageState extends State<AddProductPage> {
       appBar: AppBar(title: Text('Add Product')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
+        child: SingleChildScrollView(child: 
+        Form(
           key: _formKey,
           child: Column(
             children: <Widget>[
+              GestureDetector(
+                onTap: _pickImage,
+                child: Container(
+                  width: double.infinity,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: _image != null
+                      ? Image.file(_image!, fit: BoxFit.scaleDown)
+                      : Icon(Icons.add_a_photo, color: Colors.grey, size: 50),
+                ),
+              ),
+                TextFormField(
+                decoration: InputDecoration(labelText: 'Product ID'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a product id';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _productId = value;
+                },
+               
+              ),
+              SizedBox(height: 20,),
+             ElevatedButton.icon(
+                onPressed: () {
+                  // Button action
+                },
+                icon: Icon(Icons.qr_code), // Add icon
+                label: Text('Scan QR code'), // Button label
+                style: ElevatedButton.styleFrom(
+                  minimumSize:Size(double.infinity, 10) ,
+                  padding: EdgeInsets.symmetric(vertical: 10), // Button height
+                  textStyle: TextStyle(fontSize: 16),backgroundColor: Colors.green , 
+                  foregroundColor: Colors.black
+                ),
+              ),
+
               TextFormField(
                 decoration: InputDecoration(labelText: 'Product Name'),
                 validator: (value) {
@@ -111,31 +180,60 @@ class _AddProductPageState extends State<AddProductPage> {
                 onSaved: (value) {
                   _productDescription = value;
                 },
+              ), TextFormField(
+                decoration: InputDecoration(labelText: 'Product Status'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a product status';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _productStatus = value;
+                },
               ),
               SizedBox(height: 16.0),
-              GestureDetector(
-                onTap: _pickImage,
-                child: Container(
-                  width: double.infinity,
-                  height: 150,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: _image != null
-                      ? Image.file(_image!, fit: BoxFit.cover)
-                      : Icon(Icons.add_a_photo, color: Colors.grey, size: 50),
+              
+              SizedBox(height: 16.0),
+              DropdownButtonFormField(
+                decoration: InputDecoration(labelText: 'Product Family'),
+                value: _selectedProductFamily,
+                items: _productFamilies.map((family) {
+                  return DropdownMenuItem(
+                    value: family.id.toString(),
+                    child: Text(family.name),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    _selectedProductFamily = value as String?;
+                  });
+                  
+                },
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please select a product family';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20,),
+              ElevatedButton.icon(
+                label: Text("Add Product"),
+                icon:  Icon(Icons.add),
+                style: ElevatedButton.styleFrom(
+                 
+                  minimumSize:Size(double.infinity, 10) ,
+                  padding: EdgeInsets.symmetric(vertical: 25), // Button height
+                  textStyle: TextStyle(fontSize: 16),backgroundColor: Colors.green , 
+                  foregroundColor: Colors.black
                 ),
-              ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
                 onPressed: _submitForm,
-                child: Text('Submit'),
               ),
             ],
           ),
         ),
       ),
-    );
+    ));
   }
 }
