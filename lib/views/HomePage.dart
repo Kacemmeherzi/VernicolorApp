fda664e0250e368d0005fe29c6fdf3078efba81e
kacem <kacemmeherzi52@gmail.com>
2024-10-04 12:14:35 +0100

    homepage redesgined

--- lib/views/HomePage.dart
+++ lib/views/HomePage.dart
@@ -1,75 +1,145 @@
 import 'package:flutter/material.dart';
 import 'package:vernicolorapp/views/AddCustomerPage.dart';
 import 'package:vernicolorapp/views/AddProductFamilyPage.dart';
+import 'package:vernicolorapp/views/AddProductPage.dart';
 import 'package:vernicolorapp/views/CustomerListPage.dart';
 import 'package:vernicolorapp/views/ProductFamilyListPage.dart';
 import 'package:vernicolorapp/views/ProductListPage.dart';
-
+import 'package:vernicolorapp/widgets/customButton.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Text('Vernicolor App'),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
           children: [
-            ElevatedButton(
-              onPressed: () {
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blueAccent,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.list),
+              title: Text('View Products'),
+              onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => ProductListPage()),
                 );
               },
-              child: Text('View Products'),
             ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
+            ListTile(
+              leading: Icon(Icons.people),
+              title: Text('View Customers'),
+              onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => CustomerListPage()),
                 );
               },
-              child: Text('View Customers'),
             ),
-              SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
+            ListTile(
+              leading: Icon(Icons.category),
+              title: Text('View Product Families'),
+              onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => ProductFamilyListPage()),
                 );
               },
-              child: Text('View Product families'),
             ),
-            
-              SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
+            ListTile(
+              leading: Icon(Icons.person_add),
+              title: Text('Add a New Customer'),
+              onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => AddCustomerPage()),
                 );
               },
-              child: Text('Add a new Customer '),
             ),
-              SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
+            ListTile(
+              leading: Icon(Icons.add_business),
+              title: Text('Add a New Product Family'),
+              onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => AddProductFamilyPage()),
                 );
               },
-              child: Text('Add a new Product family  '),
+            ),
+            ListTile(
+              leading: Icon(Icons.add),
+              title: Text('Add a New Product'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AddProductPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            buildButton(
+              context,
+              'View Products',
+              Icons.list_alt,
+              ProductListPage(),
+            ),
+            buildButton(
+              context,
+              'View Customers',
+              Icons.people,
+              CustomerListPage(),
+            ),
+            buildButton(
+              context,
+              'View Product Families',
+              Icons.category,
+              ProductFamilyListPage(),
+            ),
+            buildButton(
+              context,
+              'Add a New Customer',
+              Icons.person_add,
+              AddCustomerPage(),
+            ),
+            buildButton(
+              context,
+              'Add a New Product Family',
+              Icons.add_business,
+              AddProductFamilyPage(),
+            ),
+            buildButton(
+              context,
+              'Add a New Product',
+              Icons.add_box,
+              AddProductPage(),
             ),
           ],
         ),
       ),
     );
   }
+
+ 
 }
